0f2e1d20d1783cd229c572ce0d92bb251a876bdf
liwenxuan <qwerty001lwx@gmail.com>
2019-04-08 13:11:19 +1000

    improved

--- assignment1/workload2/reducer_.py
+++ assignment1/workload2/reducer_.py
@@ -18,33 +18,33 @@ def reducer():
 
     boundary_indices = [0,]
     data_for_each_id = []
-    for index in range(1, len(data_for_each_id)):
+    for index in range(1, len(data)):
         if data[index][0] != data[index-1][0]:
             boundary_indices.append(index)
 
-    boundary_indices.append(len(data_for_each_id))
-    certain_id = []
+    boundary_indices.append(len(data))
+
+
     for i in range(len(boundary_indices)-1):
         certain_id = data[boundary_indices[i]:boundary_indices[i+1]]
         data_for_each_id.append(certain_id)
 
 
     output_data = []
-    for each in data_for_each_id:
-        if each.__len__() < 2:
-            continue
-
-        that_id = sorted(each, key=lambda s:s[1])
-        initial_like = that_id[0][3]
-        second_like = that_id[1][3]
-        initial_dislike = that_id[0][2]
-        second_dislike = certain_id[1][2]
-        if (second_dislike - init_dislike) > (second_like - init_like):
-            gap = (second_dislike - init_dislike) - (second_like - init_like)
-            if gap > 0:
-                id_and_country = that_id[0][0].split(",")
-                out = [id_and_country[0], gap, that_id[0][4], id_and_country[1]]
-                output_data.append(out)
+    print(data_for_each_id[5])
+    for i in range(len(data_for_each_id)):
+        if len(data_for_each_id[i]) >= 2:
+            that_id = data_for_each_id[i]
+            init_like = int(that_id[0][3])
+            second_like = int(that_id[1][3])
+            init_dislike = int(that_id[0][2])
+            second_dislike = int(that_id[1][2])
+            if (second_dislike - init_dislike) > (second_like - init_like):
+                gap = (second_dislike - init_dislike) - (second_like - init_like)
+                if gap > 0:
+                    id_and_country = that_id[0][0].split(",")
+                    out = [id_and_country[0], gap, that_id[0][4], id_and_country[1]]
+                    output_data.append(out)
 
 
     top10 = sorted(output_data, key=lambda s:s[1], reverse=True)[:10]
@@ -56,4 +56,3 @@ def reducer():
 
 if __name__ == '__main__':
     reducer()
-c
