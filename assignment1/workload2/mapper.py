889e71fa9367b8cad93fdeb850e533eaf2542345
liwenxuan <qwerty001lwx@gmail.com>
2019-04-09 13:22:00 +1000

    workload2 annotated

--- assignment1/workload2/mapper.py
+++ assignment1/workload2/mapper.py
@@ -17,8 +17,10 @@ def mapper():
             continue
 
         idandcountry = video_id+","+country
+        # to connect id and country with a comma, and later they'll be separeted.
         trending_date = re.sub("\D", "", trending_dates)
         str2 = trending_date[:-4] + trending_date[-2:] + trending_date[-4:-2]
+        # to convert the pure digital date from yy/dd/mm yo yy/mm/dd
         print("{}\t{}\t{}\t{}\t{}".format(idandcountry, str2, likes, dislikes, category))
 
 
--- assignment1/workload2/reducer.py
+++ assignment1/workload2/reducer.py
@@ -17,7 +17,7 @@ def reducer():
     for id_country, date, like, dislike, category in read_map_output(sys.stdin):
         each = [id_country, date, dislike, like, category]
         data.append(each)
-
+    # make a list which contains each line in the input also as a list
     boundary_indices = [0,]
     data_for_each_id = []
     for index in range(1, len(data)):
@@ -25,17 +25,20 @@ def reducer():
             boundary_indices.append(index)
 
     boundary_indices.append(len(data))
-
+    # get all boundaries of different video_id,
+    # for convenience in later computation, added 0 as the first boundary and the length of input data as the final boundary
 
     for i in range(len(boundary_indices)-1):
         certain_id = data[boundary_indices[i]:boundary_indices[i+1]]
         data_for_each_id.append(certain_id)
-
+    # to seperate the data for each video_id
 
     output_data = []
     for i in range(len(data_for_each_id)):
         if len(data_for_each_id[i]) >= 2:
+            # video_id that has less than 2 records cannot be calculate and hence ignored
             that_id = sorted(data_for_each_id[i], key=lambda s:s[1])
+            #sort all record of a certain video_id by date successively
             init_like = int(that_id[0][3])
             second_like = int(that_id[1][3])
             init_dislike = int(that_id[0][2])
