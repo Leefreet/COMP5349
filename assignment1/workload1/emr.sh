99fc00c444c695463cd01fe757f07a5b27d86689
liwenxuan <qwerty001lwx@gmail.com>
2019-04-09 12:37:51 +1000

    workload1 shell finished

--- assignment1/workload1/emr.sh
+++ assignment1/workload1/emr.sh
@@ -3,8 +3,8 @@
 spark-submit \
     --master local[4] \
     main.py \
-    --input /home/hadoop/asm1/ \
-    --output /home/hadoop/asm1/rating_out/
+    --input /home/hadoop/COMP5349/assignment1/ \
+    --output /home/hadoop/COMP5349/out/
 	 
 
     
