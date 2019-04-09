ba79d9e4ab7fcc8587313681b1e3b48b35cf4621
liwenxuan <qwerty001lwx@gmail.com>
2019-04-09 13:08:57 +1000

    workload1 finished

--- assignment1/workload1/main.py
+++ assignment1/workload1/main.py
@@ -7,16 +7,16 @@ if __name__ == '__main__':
     sc = SparkContext(appName="Trending-Category Correlation")
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help="the input path",
-                        default='~/asm1/')
+                        default='~/COMP5349/assignment1/')
     parser.add_argument("--output", help="the output path",
-                        default='~/asm1/out_script/')
+                        default='~/COMP5349/out/')
     args = parser.parse_args()
     input_path = args.input
     output_path = args.output
     text = sc.textFile(input_path+'AllVideos_short.csv')
     raw_data = text.map(extract)
     header = raw_data.first()
-    valid = raw_data.filter(lambda x:x!=header)
+    valid = raw_data.filter(lambda s:s!=header) # to remove the first line of the input .csv
 
     data = valid.reduceByKey(countries).map(get_length)
 
--- assignment1/workload1/operations.py
+++ assignment1/workload1/operations.py
@@ -1,6 +1,10 @@
 import csv
 
 def extract(record):
+    """
+    :param record:input data
+    :return: a tuple in form of ((category, video_id), country)
+    """
     try:
         row = record.split(",")
         video_id, category, country = row[0], row[3], row[-1]
@@ -11,7 +15,12 @@ def extract(record):
 
 
 def countries(countries, country):
-
+    """
+    this function is to get all countries where a certain video_id has appeared in.
+    :param countries:a string contains countries where a certain video_id has appeared in.
+    :param country: current country
+    :return: a string of conutries without duplication
+    """
     if country not in countries:
         countries = countries + "|" + country
 
@@ -19,6 +28,12 @@ def countries(countries, country):
 
 
 def id_country_number(pair, current_country_number):
+    """
+    this function is to get the amount of ids appeared in a certain category and the total amount of countries corresponding to all ids.
+    :param pair:(current total amount of countries, current id amount in a certain category)
+    :param current_country_number:
+    :return:(the cumulative sum of countries included in each id, all ids appeared in a certain category)
+    """
     total_country_number, total_id_number = pair
     total_country_number += current_country_number
     total_id_number += 1
@@ -31,19 +46,30 @@ def merge_numbers(pair1, pair2):
     return (country_number1+country_number2, id_number1+id_number2)
 
 
-def get_average_trending(line):
-    category, total_countries_ids = line
+def get_average_trending(pair):
+    category, total_countries_ids = pair
 
     average_trending = total_countries_ids[0] / total_countries_ids[1]
     return (category, round(average_trending,2))
 
 
-def get_length(tuple):
-    country_list = tuple[1].strip().split("|")
-    return (tuple[0], len(set(country_list)))
+def get_length(pair):
+    """
+    this function is to get the amount of countries in the string.
+    :param pair: ((category, video_id), countries string)
+    :return: ((category, video_id), number of countries)
+    """
+    country_list = pair[1].strip().split("|")
+    return (pair[0], len(set(country_list)))
 
 
-def remove_id(tuple):
-    t0, t1 = tuple[0], tuple[1]
+def remove_id(pair):
+    """
+    this function is to remove video_id from key tuple, after this operation, the number of times that a certain category appears
+    can replace the number of ids within this category.
+    :param pair: ((category, video_id), number of countries)
+    :return: (a category, number of countries )
+    """
+    t0, t1 = pair[0], pair[1]
     new_t0 = t0[0]
     return (new_t0, t1)
